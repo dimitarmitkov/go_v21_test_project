e88780f4e0e8cace02cc1e5fa94fca2bb684fa13
dimitarmitkov <dimitar.mitkov@me.com>
2025-01-21 16:26:51 +0200

    v21 code changes, min max and slices

--- minmax.go
+++ minmax.go
@@ -3,12 +3,33 @@ package main
 import "fmt"
 
 func MinMax() {
-	m := min(10, 20)
-	fmt.Printf("shows min of 10 and 20: %d\n", m)
+	mMin := min(10, 20)
+	fmt.Printf("shows min of 10 and 20: %d\n", mMin)
 
-	m = max(10, 20)
-	fmt.Printf("shows max of 10 and 20: %d\n", m)
+	mMax := max(10, 20)
+	fmt.Printf("shows max of 10 and 20: %d\n", mMax)
 
-	m = max(9, 5, 10)
-	fmt.Printf("shows max of 5 and 9, but at least 10: %d\n", m)
+	m10 := max(9, 5, 10)
+	fmt.Printf("shows max of 5 and 9, but at least 10: %d\n", m10)
+
+	mm10 := min(9, 5, 2)
+	fmt.Println("min of 9 and 5, but at least 2: ", mm10)
+
+	s := max("fed", "def")
+	fmt.Println("shows min of 'abc' and 'def'", s)
+
+	sCap := max("ABC", "abc")
+	fmt.Println("shows max between 'ABC' and 'abc'", sCap)
+
+	sf := min("aaa", "def", "abc")
+	fmt.Println("shows min of 'abc' and 'def'", sf)
+
+	flt := min(1, 1.2)
+	fmt.Println("min of float values: ", flt)
+
+	fltmx := max(1, 1.2)
+	fmt.Println("min of float values: ", fltmx)
+
+	fltmx = max(1, 1.2, 3.2)
+	fmt.Println("min of float values: (3.2) ", fltmx)
 }
--- slices.go
+++ slices.go
@@ -10,6 +10,7 @@ func SlicesExamples() {
 	exampleSliceAddSix := []string{"one", "two", "three", "four", "five", "six"}
 	exampleSliceCopyTrue := []string{"one", "two", "three", "four", "five"}
 
+	//Contains and Equal
 	doesSliceContainsOne := slices.Contains(exampleSlice, "one")
 	areSlicesEqualFalse := slices.Equal(exampleSlice, exampleSliceAddSix)
 	areSlicesEqualTrue := slices.Equal(exampleSlice, exampleSliceCopyTrue)
@@ -17,4 +18,94 @@ func SlicesExamples() {
 	fmt.Printf("check if slice contains element, should be true: %t\n", doesSliceContainsOne)
 	fmt.Printf("are slices equal, should be false: %t\n", areSlicesEqualFalse)
 	fmt.Printf("are slices equal, should be true: %t\n", areSlicesEqualTrue)
+
+	// Delete
+	exampleSliceAddSixDeletable := []string{"one", "two", "three", "four", "five", "six"}
+	newSliceAfterDelete := slices.Delete(exampleSliceAddSixDeletable, 0, 2)
+	fmt.Println("slice after deletion last element: ", newSliceAfterDelete)
+	fmt.Println("slice after deletion last element length: ", len(newSliceAfterDelete), cap(newSliceAfterDelete))
+	fmt.Println("original slice and its length : ", exampleSliceAddSixDeletable, len(exampleSliceAddSixDeletable), cap(exampleSliceAddSixDeletable))
+
+	// Clip
+	newSliceAfterDelete = slices.Clip(newSliceAfterDelete)
+	fmt.Println("slice after deletion last element length and fix it by Clip: ", len(newSliceAfterDelete), cap(newSliceAfterDelete))
+
+	// Clone
+	originalCloneableSlice := []string{"A", "B", "C", "D", "E", "F", "G"}
+	clonedSliceFromOriginal := slices.Clone(originalCloneableSlice)
+
+	fmt.Println("original:", originalCloneableSlice)
+	fmt.Println("clone:", clonedSliceFromOriginal)
+
+	clonedSliceFromOriginal[0] = "Q"
+	fmt.Println("original:", originalCloneableSlice)
+	fmt.Println("clone:", clonedSliceFromOriginal)
+
+	// Compact
+	sliceToBeCompacted := []int{1, 1, 2, 2, 3, 3, 4, 5, 6, 6, 6, 7, 7, 7, 7, 7, 7, 8}
+	fmt.Println(sliceToBeCompacted)
+
+	compactedSlice := slices.Compact(sliceToBeCompacted)
+	fmt.Println(compactedSlice)
+	fmt.Println(sliceToBeCompacted)
+
+	// compact doesn't work in slice like
+	stc := []int{1, 2, 3, 1, 2, 3, 1, 2, 3}
+	cts := slices.Compact(stc)
+
+	fmt.Println(stc)
+	fmt.Println(cts)
+
+	index5, found5 := slices.BinarySearch(sliceToBeCompacted, 5)
+	fmt.Println("found 5: ", index5, found5)
+	index7, found7 := slices.BinarySearch(sliceToBeCompacted, 7)
+	fmt.Println("found 7: ", index7, found7)
+	index8, found8 := slices.BinarySearch(sliceToBeCompacted, 8)
+	fmt.Println("found 7: ", index8, found8)
+
+	// Sort
+	slices.Sort(stc)
+	fmt.Println(stc)
+
+	// Max Min
+	maxValue := slices.Max(sliceToBeCompacted)
+	fmt.Println("max value: ", maxValue)
+	minValue := slices.Min(sliceToBeCompacted)
+	fmt.Println("min value: ", minValue)
+
+	// important Index doesn't have in ming slice len
+	indexValue := slices.Index(sliceToBeCompacted, 8)
+	fmt.Println("index: ", indexValue)
+	fmt.Println("cap, len: ", cap(sliceToBeCompacted), len(sliceToBeCompacted))
+
+	// Grow
+	initialSliceToBeGrown := []int{1, 1, 2, 2, 3, 3, 4, 5, 6, 6, 6, 7, 7, 7, 7, 7, 7, 8}
+	fmt.Println("cap, len before grow with 10: ", cap(initialSliceToBeGrown), len(initialSliceToBeGrown))
+	grownSlice := slices.Grow(initialSliceToBeGrown, 25)
+	fmt.Println("cap, len after grow with 10: ", cap(grownSlice), len(grownSlice))
+	fmt.Println(grownSlice)
+
+	// Reverse slice
+	slices.Reverse(initialSliceToBeGrown)
+	fmt.Println("reversed slice: ", initialSliceToBeGrown)
+
+	// Replace
+	names := []string{"Alice", "Bob", "Vera", "Zac"}
+	names = slices.Replace(names, 1, 3, "Bill", "Billie", "Cat")
+	fmt.Println(names)
+
+	nums := []int{1, 2, 3, 4}
+	nums = slices.Replace(nums, 1, 2, 44, 45)
+	fmt.Println(nums)
+
+	// Insert and IsSorted
+	notSorted := []int{1, 3, 45, 2, 11, 5, 66, 778, 10}
+	isSortedCheck := slices.IsSorted(notSorted)
+	fmt.Println("check if is sorted: ", isSortedCheck)
+	slices.Sort(notSorted)
+	isSortedCheck = slices.IsSorted(notSorted)
+	fmt.Println("check if is sorted (after sort): ", isSortedCheck)
+
+	notSorted = slices.Insert(notSorted, 1, 100)
+	fmt.Println("inserted 100: ", notSorted)
 }
